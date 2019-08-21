2ba1c14c1b9316247a357ec1896e4e070ca9a0b8
Kari Hodge <kari-pmgchat@malachiarts.com>
2019-08-20 19:56:26 -0700

    generate link resource

--- actions/app.go
+++ actions/app.go
@@ -76,6 +76,7 @@ func App() *buffalo.App {
 		app.POST("/signin", AuthCreate)
 		app.DELETE("/signout", AuthDestroy)
 		app.Middleware.Skip(Authorize, HomeHandler, UsersNew, UsersCreate, AuthNew, AuthCreate)
+		app.Resource("/links", LinksResource{})
 		app.ServeFiles("/", assetsBox) // serve files from the public directory
 	}
 
